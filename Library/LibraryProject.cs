703621419b26fb461b1679cc3736d6c0bfcf9ca9
AKaurManpreet <156477920+AKaurManpreet@users.noreply.github.com>
2024-12-13 21:33:40 -0500

    Add initial implementation of Library project with Printer class and project files

---
+++ Library/LibraryProject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryProject
+{
+    public class Printer
+    {
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
